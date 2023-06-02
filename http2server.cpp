0ba80f568c6e45c79d57356b107bf985b0320e74
CC <3181792344@qq.com>
2023-06-02 17:59:42 +0800

    Added http2 process function

--- http2server.cpp
+++ http2server.cpp
@@ -4,3 +4,20 @@ Http2Server::Http2Server()
 {
 
 }
+void Http2Server::process(Connection &sock)
+{
+    Requset req;
+    req.timeout=std::chrono::milliseconds(15000);
+
+}
+
+void Http2Server::send_empty_settings(const Connection &sock,const std::chrono::milliseconds &timeout,
+                                      const headerframe_flag flags)
+{
+    constexpr uint32_t frame_size=0;//该帧的长度
+    constexpr uint32_t frame_header_size=9;//头部帧长度
+    std::array<uint8_t,frame_header_size+frame_size> buf;
+    uint8_t *addr=buf.data();
+    constexpr uint32_t stream_id=0;
+
+}
--- http2server.h
+++ http2server.h
@@ -1,11 +1,26 @@
 #ifndef HTTP2SERVER_H
 #define HTTP2SERVER_H
 
+#include<string.h>
+#include"connection.h"
+#include"frame.h"
+struct Requset{
+    std::unordered_map<std::string,std::string> outgoing_headers;
+    std::string host;
+    std::string path;
+    std::string method;
+    std::chrono::milliseconds timeout;//超时时间
+
+};
 
 class Http2Server
 {
 public:
     Http2Server();
+    void process(Connection &sock);//http2处理的所有过程
+    static void send_empty_settings(const Connection &sock,const std::chrono::milliseconds &timeout,
+                                    const headerframe_flag flags);//发送空的settings帧
+
 };
 
 #endif // HTTP2SERVER_H
